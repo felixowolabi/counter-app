7ca37ea45f17d1f3add53305918894856aa79874
felixowolabi <flo.owol@gmail.com>
2023-01-03 23:03:12 +0200

    Update App.js
    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((firstCount) => firstCount + 1);
+  }
+
+  function decrement() {
+    setCount((firstCount) => firstCount - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </>
   );
 }
-
-export default App;
